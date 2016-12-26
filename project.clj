07f288ba376afacc88d36bdb94e3664ddb71732c
Christian Weilbach <christian@topiq.es>
2016-12-26 15:29:58 +0100

    Bump deps.

--- project.clj
+++ project.clj
@@ -1,21 +1,20 @@
-(defproject io.replikativ/kabel "0.1.9"
+(defproject io.replikativ/kabel "0.1.10"
   :description "A library for simple wire-like connectivity semantics."
   :url "https://github.com/replikativ/kabel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/clojurescript "1.9.229"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.229" :scope "provided"]
                  [io.replikativ/superv.async "0.2.2"]
-                 [io.replikativ/incognito "0.2.0"]
 
-                 [http-kit "2.2.0"]
+                 [com.cognitect/transit-clj "0.8.285"]
+                 [com.cognitect/transit-cljs "0.8.239"]
+                 [io.replikativ/incognito "0.2.1"]
+
+                 [io.replikativ/hasch "0.3.4"]
+
+                 [org.slf4j/slf4j-api "1.7.12"] ;; TODO factor logging
+
+                 [http-kit "2.2.0"] ;; TODO factor those as scope provided
                  [http.async.client "1.2.0"]
-                 [aleph "0.4.2-alpha8"]
-
-                 [com.cognitect/transit-cljs "0.8.239"] ;; TODO remove
-                                                        ;; once cljs
-                                                        ;; works again
-                                                        ;; without it
-                 [io.replikativ/hasch "0.3.2"]
-
-                 [org.slf4j/slf4j-api "1.7.12"]])
+                 [aleph "0.4.2-alpha8"]])

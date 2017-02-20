2a938ccba34d20ef7346749009abdd985ec457f2
Christian Weilbach <christian@topiq.es>
2017-02-20 20:39:59 +0100

    Use different ports.

--- test/kabel/core_test.clj
+++ test/kabel/core_test.clj
@@ -46,7 +46,7 @@
   (testing "Pushing a thousand messages through."
     (let [sid #uuid "fd0278e4-081c-4925-abb9-ff4210be271b"
           cid #uuid "898dcf36-e07a-4338-92fd-f818d573444a"
-          url "ws://localhost:47291"
+          url "ws://localhost:47292"
           handler (http-kit/create-http-kit-handler! S url sid)
           speer (peer/server-peer S handler sid pong-middleware identity)
           cpeer (peer/client-peer S cid (fn [[S peer [in out]]]
